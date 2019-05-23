685ff42989d856101450353053a4c0b12bbe09f1
Atif Aziz <code@raboof.com>
2019-05-23 17:20:34 +0200

    Initial import

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpMinifierDemo
+{
+    using Microsoft.AspNetCore.Blazor.Hosting;
+    using Microsoft.AspNetCore.Components.Builder;
+    using Microsoft.Extensions.DependencyInjection;
+
+    static class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost
+                .CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+
+    public sealed class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) {}
+
+        public void Configure(IComponentsApplicationBuilder app) =>
+            app.AddComponent<App>("app");
+    }
+}
