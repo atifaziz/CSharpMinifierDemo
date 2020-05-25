3d231da296409f39148dde64ab1a71c17d14d1c9
Atif Aziz <code@raboof.com>
2020-05-25 15:09:57 +0200

    Update to Blazor WebAssembly 3.2.0 release (#3)

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
 ﻿namespace CSharpMinifierDemo
 {
-    using System.Linq;
-    using Microsoft.AspNetCore.Blazor.Hosting;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Components.Builder;
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
 
     static class Program
     {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost
-                .CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
-    }
-
-    public sealed class Startup
-    {
-        public void ConfigureServices(IServiceCollection services) =>
-            services.AddResponseCompression(options =>
-                options.MimeTypes.Append("application/octet-stream")
-                                 .Append("application/wasm"));
-
-        public void Configure(IComponentsApplicationBuilder app) =>
-            app.AddComponent<App>("app");
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            var baseUrl = new Uri(builder.HostEnvironment.BaseAddress);
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = baseUrl });
+            await builder.Build().RunAsync();
+        }
     }
 }
